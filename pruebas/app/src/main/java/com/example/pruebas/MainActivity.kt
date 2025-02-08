cc4321058de9e0e6913933a70cfd77fbef92c0a2
modinoricardo <modinoricardo@gmail.com>
2025-02-08 23:10:33 +0100

    Final dia

---
+++ pruebas/app/src/main/java/com/example/pruebas/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.pruebas
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var spiner:Spinner
+    lateinit var spiner2:Spinner
+
+    var paises = arrayOf<String>("España", "Francia","Italia")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        spiner = findViewById(R.id.spinner)
+        spiner2 = findViewById(R.id.spinner2)
+
+        var miAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, paises)
+        miAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spiner.adapter = miAdapter
+
+        spiner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+    }
+
+}

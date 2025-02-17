73bc2e6b722b3a53f9b0c4dda51e8d83f793903f
modinoricardo <modinoricardo@gmail.com>
2025-02-17 14:20:13 +0100

    Final casa

--- JugandoALosDados/app/src/main/java/com/example/jugandoalosdados/MainActivity.kt
+++ JugandoALosDados/app/src/main/java/com/example/jugandoalosdados/MainActivity.kt
@@ -1,28 +1,34 @@
 package com.example.jugandoalosdados
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.jugandoalosdados.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var textSaldo:TextView
+    lateinit var binding: ActivityMainBinding
+
+    var saldo = 100
+    var varParImpar = listOf("PAR","IMPAR")
+    var varMayorMenorSeven = listOf("MAYOR O IGUAL QUE 7","MENOR QUE 7","IGUAL A 7")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        textSaldo = findViewById(R.id.textView2)
-        textSaldo.text = 100.toString()
+        with(binding){
+            textSaldo.text = saldo.toString()
+        }
 
     }
 }

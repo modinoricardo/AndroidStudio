3077b1445b8b50b8d9b2322afce90602622cf491
modinoricardo <modinoricardo@gmail.com>
2025-02-20 15:19:18 +0100

    Finla clases

--- JugandoALosDados/app/src/main/java/com/example/jugandoalosdados/MainActivity.kt
+++ JugandoALosDados/app/src/main/java/com/example/jugandoalosdados/MainActivity.kt
@@ -1,16 +1,20 @@
     package com.example.jugandoalosdados
 
+    import android.annotation.SuppressLint
     import android.os.Bundle
-    import android.util.Log
     import android.view.View
-    import android.widget.AdapterView
     import android.widget.ArrayAdapter
     import androidx.activity.enableEdgeToEdge
     import androidx.appcompat.app.AppCompatActivity
     import androidx.core.view.ViewCompat
     import androidx.core.view.WindowInsetsCompat
-    import androidx.core.view.get
+    import androidx.lifecycle.lifecycleScope
+    import com.bumptech.glide.Glide
     import com.example.jugandoalosdados.databinding.ActivityMainBinding
+    import com.google.android.material.snackbar.Snackbar
+    import kotlinx.coroutines.delay
+    import kotlinx.coroutines.launch
+    import kotlin.random.Random
 
     class MainActivity : AppCompatActivity() {
 
@@ -19,6 +23,8 @@
         var saldo = 100
         var listParImpar = listOf("Par","Impar")
         var listMayorMenorSeven = listOf("Mayor o igual que 7","Menor que 7")
+        var dado1:Int = 0
+        var dado2:Int = 0
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
@@ -33,41 +39,131 @@
             }
 
             with(binding){
-                textSaldo.text = saldo.toString()
+                textSaldo.text = "$saldo"
 
                 parImparButton.setOnClickListener{
-                    botonParSeleccionado(true);
+                    botonParSeleccionado(true)
                 }
 
                 mayorMenorButton.setOnClickListener {
-                    botonParSeleccionado(false);
+                    botonParSeleccionado(false)
                 }
 
                 buttonLanzarDados.setOnClickListener{
-                    lanzarDados();
+                    lifecycleScope.launch {
+                        lanzarDados()
+                    }
                 }
 
             }
 
         }
 
-        private fun validaLanzarDados(): Boolean {
-            return binding.parImparButton.isActivated && binding.miSpinner.isActivated
+        private fun validaSeleccionDatos():Int{
+            if (binding.toggleButton.checkedButtonId == View.NO_ID){
+                mensajeSnackbar("Debe elegir una opcion de juego")
+                return -1
+            }else if(binding.editTextNumberApuesta.text.isNullOrBlank()){
+                mensajeSnackbar("Debe introducir una apuesta")
+                return -1
+            }else{
+                return 0
+            }
         }
 
-        private fun lanzarDados() {
-            if(!validaLanzarDados()){
-
+        private fun validarSaldoApuesta(): Boolean {
+            val apuesta = binding.editTextNumberApuesta.text.toString().toInt()
+            if(apuesta > saldo){
+                mensajeSnackbar("La apuesta es mayor al saldo disponible")
+                return false
             }else{
+                return true
+            }
+        }
+
+        @SuppressLint("SetTextI18n")
+        private suspend fun lanzarDados() {
+            limpiarVista()
+
+            if(validaSeleccionDatos() == 0 && validarSaldoApuesta()){
+                Glide.with(this).load(R.drawable.dadtos2).into(binding.imageView)
+                delay(3000)
+
+                dado1 = (1..6).random()
+                dado2 = (1..6).random()
+
+                binding.imageView.setImageDrawable(null)
+
+                binding.textViewApuestaUno.text = "Dado_1:  $dado1"
+                binding.textViewApuestaDos.text = "Dado_2:  $dado2"
+
+                if(binding.toggleButton.checkedButtonId == R.id.parImparButton){
+                    parImpar()
+                }else{
+                    mayorMenor7()
+                }
 
             }
+        }
 
+        private fun limpiarVista() {
+            binding.imageViewRespuesta.visibility = View.INVISIBLE
+            binding.textViewApuestaUno.text = ""
+            binding.textViewApuestaDos.text = ""
+        }
+
+        private fun menor7() = (dado1 + dado2)<7
+        private fun mayorIgual7() = (dado1 + dado2)>=7
+        
+        private fun mayorMenor7() {
+            var gano = false
+            when(binding.miSpinner.selectedItemPosition){
+                0 -> gano = mayorIgual7()
+                1 -> gano= menor7()
+            }
+
+            if(gano) partidaGanada() else partidaPerdida()
+
+        }
+
+        private fun impar() = (dado1 + dado2)%2!=0
+        private fun par() = (dado1 + dado2)%2==0
+        private fun parImpar() {
+            var gano = false
+            when(binding.miSpinner.selectedItemPosition){
+                0 -> gano = par()
+                1 -> gano = impar()
+            }
+            if(gano) partidaGanada() else partidaPerdida()
         }
 
         private fun botonParSeleccionado(par: Boolean) {
-            var miAdapter = ArrayAdapter(this,
+            val miAdapter = ArrayAdapter(this,
                 android.R.layout.simple_spinner_dropdown_item,if(par) listParImpar else listMayorMenorSeven)
             binding.miSpinner.adapter = miAdapter
+        }
+
+//        private fun mensajeSnackbar(mensaje:String){
+//            Snackbar.make(binding.root, mensaje, Snackbar.LENGTH_LONG).show()
+//        }
+        private fun mensajeSnackbar(mensaje:String){
+            Snackbar.make(binding.coordinatorLayout, mensaje, Snackbar.LENGTH_LONG).show()
+        }
+
+        private fun partidaGanada(){
+            var apuesta = binding.editTextNumberApuesta.text.toString().toInt()
+            saldo += apuesta
+            binding.textSaldo.text= saldo.toString()
+            binding.imageViewRespuesta.visibility = View.VISIBLE
+            binding.imageViewRespuesta.setImageResource(R.drawable.win)
+        }
+
+        private fun partidaPerdida(){
+            var apuesta = binding.editTextNumberApuesta.text.toString().toInt()
+            saldo -= apuesta
+            binding.textSaldo.text= saldo.toString()
+            binding.imageViewRespuesta.visibility = View.VISIBLE
+            binding.imageViewRespuesta.setImageResource(R.drawable.youlose)
         }
 
     }

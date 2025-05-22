45bc8f554bc3ea6d23b302c3e5db46d005cb83bf
modinoricardo <modinoricardo@gmail.com>
2025-05-22 16:09:14 +0200

    Aplicacion terminada correctamente

--- ProyectoFinalRicardoMiTienda/app/src/main/java/com/example/proyectofinalricardomitienda/adapter/ProductAdapter.kt
+++ ProyectoFinalRicardoMiTienda/app/src/main/java/com/example/proyectofinalricardomitienda/adapter/ProductAdapter.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.proyectofinalricardomitienda.R
 import com.example.proyectofinalricardomitienda.activity.ProductDetailActivity
 import com.example.proyectofinalricardomitienda.entities.Product
@@ -31,19 +32,26 @@ class ProductAdapter() : RecyclerView.Adapter<ProductView>() {
         val product = miListaProductos[position]
 
         // 1) Construye la URL completa de forma segura:
-        val base = Util.URL.removeSuffix("/")        // "http://10.0.2.2:8000/test/app"
+//        val base = Util.URL.removeSuffix("/")        // "http://10.0.2.2:8000/test/app"
+        val base = Util.URL2.removeSuffix("/")
         val path = product.imageUrl.removePrefix("/") // "img/products/…"
         val fullUrl = "$base/$path"
+//        val fullUrl = "http://192.168.1.135:8000/test/app/img/products/coffees/cappuccino.jpg"
 
         Log.d("ProductAdapter", "Cargando imagen: $fullUrl")
 
+        val options = RequestOptions()
+            .disallowHardwareConfig()
         // 2) Carga la imagen con placeholder, error y fallback:
         Log.i("image",fullUrl)
-        Glide.with(holder.productImage.context)
+        //No funciona con la url 10.0.2.2 asi que necesito usar la ip del ordenador,
+        //si estuviera desplegado en azure el problema se solucionaria por poner la ip del servidor desplegado
+        Glide.with(myContexto)
             .load(fullUrl)
-            .placeholder(R.drawable.image_carga)
-            .error(R.drawable.broken_image)
-            .fallback(R.drawable.image_carga)
+            .apply(options)
+//            .placeholder(R.drawable.image_carga)
+//            .error(R.drawable.broken_image)
+//            .fallback(R.drawable.image_carga)
             .into(holder.productImage)
 
         // 3) Textos y click:
@@ -67,24 +75,10 @@ class ProductAdapter() : RecyclerView.Adapter<ProductView>() {
         }
     }
 
-    /**
-     * Añade nuevos productos al final y notifica la inserción.
-     */
-//    fun addProducts(newItems: ResponseProduct) {
-//        val start = items.size
-//        items.addAll(newItems)
-//        notifyItemRangeInserted(start, newItems.size)
-//    }
-
-    /**
-     * Reemplaza por completo la lista de productos.
-     */
     fun updateListProduct(newItems: List<Product>) {
         val startPosition = miListaProductos.size
         miListaProductos.addAll(newItems)
         notifyItemRangeInserted(startPosition, newItems.size)
     }
-
-
 
 }
--- ProyectoFinalRicardoMiTienda/app/src/main/java/com/example/proyectofinalricardomitienda/util/Util.kt
+++ ProyectoFinalRicardoMiTienda/app/src/main/java/com/example/proyectofinalricardomitienda/util/Util.kt
@@ -2,6 +2,7 @@ package com.example.proyectofinalricardomitienda.util
 
 object Util {
     var URL = "http://10.0.2.2:8000/test/app/"
+    var URL2 = "http://192.168.1.135:8000/test/app/"
     var accessToken= ""
     var refrehToken =""
     var username = ""
